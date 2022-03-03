60156a12a1d98e81556b71c7224cf090f4ef3617
titix <titixlodigiani@gmail.com>
2022-03-03 00:26:21 -0300

    productos,carrito

--- js/carrito.js
+++ js/carrito.js
@@ -1,32 +1,37 @@
 
 let arrayCarrito = JSON.parse(localStorage.getItem("carrito"))
-const sumar = (a,b) => a+b
-const restar = (a,b) => a-b
-let total = 0 
+const sumar = (a, b) => a + b
+const restar = (a, b) => a - b
+let total = 0
 
-for( producto of arrayCarrito){
-$("#agregados").prepend ("<li> <h6> "+ producto.producto.nombre + "  $" + producto.producto.precio + "<input class=checkboxCarrito type=checkbox checked> </h6> </li>");
+for (producto of arrayCarrito) {
+    $("#agregados").prepend("<li> <h6> " + producto.nombre + "  $" + producto.precio + "<input class=checkboxCarrito type=checkbox value=" + producto.precio + " checked> </h6> </li>");
 
-total= sumar(total,producto.producto.precio)
+    total = sumar(total, producto.precio)
 }
+
+
+
 $("#total").html("Total $" + total)
 let checkboxCarrito = $(".checkboxCarrito")
-
 checkboxCarrito.addClass("form-check-input  me-1 ")
-/*checkboxCarrito.change(function(){
-    for(let i=0; iproducto.lenght;i++){
-        console.log(i)
-        checkboxCarrito[i].click(function () {
-            if(checkboxCarrito.prop("checked")== true) {
-                total= sumar(total,producto.producto[i].precio)
-                $("#total").html("Total $" + total)
-        }
-        if(cuadrado.prop("checked")== false){
-            total= restar(total,producto.producto.precio)
-            $("#total").html("Total $" + total)
-        }})}})*/
 
-    
+checkboxCarrito.click(function (e) {
+
+    let monto = e.target.value
+    monto = parseInt(monto)
+
+    if (e.target.checked) {
+        total = sumar(total, monto)
+        $("#total").html("Total $" + total)
+    }
+    else {
+        total = restar(total, monto)
+        $("#total").html("Total $" + total)
+    }
+})
+
+
 
 
 
--- js/productos.js
+++ js/productos.js
@@ -1,25 +1,3 @@
-//productos
-
-function productos (nombre, precio) {
-    this.nombre = nombre;
-    this.precio = precio;
-}
-
-const arrayProducto = []
-arrayProducto.push (new productos ("Milanesas de Soja de Queso y Cebolla x 4 Unidades - Prasat", 300))
-arrayProducto.push (new productos ("Empanadas de Verdura Sin TACC x 2 Unidades - La Delfina", 200))
-arrayProducto.push (new productos ("Jugo 100% Exprimido Manzana Roja y Verde x 1000 ml - PURA FRUTTA", 60))
-arrayProducto.push (new productos ("Not Milk Original x 1lt - NotCo", 320))
-arrayProducto.push (new productos ("Aceite De Oliva Virgen X 1lt - EL FEDERAL", 600))
-arrayProducto.push (new productos ("Lentejas 500g", 80))
-arrayProducto.push (new productos ("Arroz Yamani Integral 500g", 120))
-arrayProducto.push (new productos ("Dulce De Leche Tradicional 450g - Las Quinas", 400))
-arrayProducto.push (new productos ("Yogurt a Base de Coco Vainilla con Granola Apto APLV x 160 g CRUDDA", 230))
-arrayProducto.push (new productos ("Cookies Sabor A Elección x 200g - Delicel", 130))
-arrayProducto.push (new productos ("Barra Sabor A Elección x 42g - Íntegra", 40))
-
-
-
 
 //division por categoria
 let producto = $(".producto")
@@ -84,8 +62,9 @@ else {
 }
 });
 
-function carrito (producto) {
-    this.producto = producto;
+function carrito (nombre,precio) {
+    this.nombre = nombre;
+    this.precio = precio;
 }
 
 const arrayCarrito = []
@@ -95,15 +74,11 @@ const arrayCarrito = []
 
 let buttonAñadir = $(".buttonAñadir")
 
-buttonAñadir[0].onclick = () => { arrayCarrito.push (new carrito(arrayProducto[0])); localStorage.setItem("carrito",JSON.stringify(arrayCarrito)) }
-buttonAñadir[1].onclick = () => { arrayCarrito.push (new carrito(arrayProducto[1])); localStorage.setItem("carrito",JSON.stringify(arrayCarrito)) }
-buttonAñadir[2].onclick = () => { arrayCarrito.push (new carrito(arrayProducto[2])); localStorage.setItem("carrito",JSON.stringify(arrayCarrito)) }
-buttonAñadir[3].onclick = () => { arrayCarrito.push (new carrito(arrayProducto[3])); localStorage.setItem("carrito",JSON.stringify(arrayCarrito)) }
-buttonAñadir[4].onclick = () => { arrayCarrito.push (new carrito(arrayProducto[4])); localStorage.setItem("carrito",JSON.stringify(arrayCarrito)) }
-buttonAñadir[5].onclick = () => { arrayCarrito.push (new carrito(arrayProducto[5])); localStorage.setItem("carrito",JSON.stringify(arrayCarrito)) }
-buttonAñadir[6].onclick = () => { arrayCarrito.push (new carrito(arrayProducto[6])); localStorage.setItem("carrito",JSON.stringify(arrayCarrito)) }
-buttonAñadir[7].onclick = () => { arrayCarrito.push (new carrito(arrayProducto[7])); localStorage.setItem("carrito",JSON.stringify(arrayCarrito)) }
-buttonAñadir[8].onclick = () => { arrayCarrito.push (new carrito(arrayProducto[8])); localStorage.setItem("carrito",JSON.stringify(arrayCarrito)) }
-buttonAñadir[9].onclick = () => { arrayCarrito.push (new carrito(arrayProducto[9])); localStorage.setItem("carrito",JSON.stringify(arrayCarrito)) }
-buttonAñadir[10].onclick = () => { arrayCarrito.push (new carrito(arrayProducto[10])); localStorage.setItem("carrito",JSON.stringify(arrayCarrito))}
-
+buttonAñadir.click(function(e){
+    let precio = e.target.value
+    precio = parseInt(precio) 
+    
+    
+    arrayCarrito.push (new carrito(e.target.name, precio)); localStorage.setItem("carrito",JSON.stringify(arrayCarrito))
+    console.log(arrayCarrito)
+})
